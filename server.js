46496a5959060ae708fd3978cd3b19b4c3d4bf67
Dominique <dfoleydev@gmail.com>
2022-07-07 22:01:01 -0400

    Added variables to sever.js

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ const app = express()
 const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
+
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = '',
+    collection
