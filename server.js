9c8188932623f7d134fce1c71baf779e576b0d4c
FoleyDom <dfoleydev@gmail.com>
2022-07-11 17:43:38 -0400

    Added MongoDB connection

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
-const db,
-    dbConnectionString = process.env.DB_STRING,
-    dbName = '',
-    collection
+let db,
+	dbConnectionString = process.env.DB_STRING,
+	dbName = 'sample_mflix',
+	collection
+
+MongoClient.connect(dbConnectionString).then((client) => {
+	console.log('Connected to Database')
+	db = client.db(dbName)
+	collection = db.collection('')
+})
